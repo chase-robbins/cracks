a4177d9bf651630e2e3ebfea4360349c3182d07f
Chase Robbins <chaserobbins@me.com>
2015-03-30 17:55:07 -0700

    script

--- falldown/Assets/Scripts/BallForce.cs
+++ falldown/Assets/Scripts/BallForce.cs
@@ -4,12 +4,31 @@ using System.Collections;
 public class BallForce : MonoBehaviour {
 
 	// Use this for initialization
+	[Tooltip("This is how fast the thing moves!")]
+	public float speed = 10;
+	public bool canFly = false;
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		float y = 0;
+		float x = Input.GetAxis ("Horizontal");
+		if(canFly == true)
+		{
+			y = Input.GetAxis ("Vertical");
+		}
+		if (x != 0 || y != 0)
+		{
+			Rigidbody2D r2d = GetComponent<Rigidbody2D>();
+			Vector2 direction = r2d.velocity;
+			if(x !=0) {direction.x = x * speed; }
+			if(y !=0) {direction.y = y * speed; }
+			r2d.velocity = direction;
+
+
+
+		}
 	}
 }
