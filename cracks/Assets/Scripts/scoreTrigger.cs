9242958aae2e483876a30112ada8357a26b902cc
chase-robbins <chaserobbins@me.com>
2015-05-04 17:16:34 -0700

    1.3.0

---
+++ cracks/Assets/Scripts/scoreTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine; 
+using System.Collections;
+public class scoreTrigger : MonoBehaviour { 
+	public static int score = 0;
+	//	public int totalScore;
+	void OnTriggerEnter2D(Collider2D sweg){
+		score++;
+		//		totalScore++;
+	}
+}
