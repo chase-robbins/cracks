65a87003aafb72400f2f59fc661a727b43c57bd3
chase-robbins <chaserobbins@me.com>
2015-05-11 22:03:09 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	cracks/Assets/scene2d.unity

--- cracks/Assets/Scripts/startLocations.cs
+++ cracks/Assets/Scripts/startLocations.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class startLocations : MonoBehaviour {
-	
+	public GameObject[] array;
+
 	// Use this for initialization
 	void Start () {
 		transform.position = new Vector2 (Random.RandomRange(-5.0f, 5.0f), transform.position.y);
+		int percentage = (Random.RandomRange(0, 100));
+		Debug.Log(percentage);
+		if (percentage >= 95) {
+		
+		}
 	}
 
 }
