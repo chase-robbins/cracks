ccd88d43198106e1442fedfa6074bbeb4a5188be
chase-robbins <chaserobbins@me.com>
2015-05-19 07:43:38 -0700

    water

--- cracks/Assets/Scripts/startLocations.cs
+++ cracks/Assets/Scripts/startLocations.cs
@@ -4,14 +4,17 @@ using System.Collections;
 
 public class startLocations : MonoBehaviour {
 	public GameObject[] array;
+	public GameObject swaginabag;
+
 
 	// Use this for initialization
 	void Start () {
+		swaginabag.active = false;
 		transform.position = new Vector2 (Random.RandomRange(-5.0f, 5.0f), transform.position.y);
 		int percentage = (Random.RandomRange(0, 100));
-		Debug.Log(percentage);
-		if (percentage >= 95) {
-		
+//		Debug.Log(percentage);
+		if (percentage >= 99) {
+			swaginabag.active = true;
 		}
 	}
 
---
+++ cracks/Assets/Scripts/turtle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turtle : MonoBehaviour {
+
+	public cameraspeed crying;
+	public gameTimer tears;
+	public float startTime = 0;
+	public float waitTimeTurtle = 0;
+	public float speedChange = 2.0f;
+	public bool needToBeChecked = false;
+
+
+	void OnTriggerEnter2D () {
+		needToBeChecked = true;
+		startTime = Time.time;
+		crying.cameraSpeedY  -= speedChange;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > startTime + 3000 && needToBeChecked){
+			needToBeChecked = false;
+			crying.cameraSpeedY  += speedChange;	
+		}
+		Debug.Log ("Camera Speed is " + crying.cameraSpeedY);	
+	}
+}
